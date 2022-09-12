2259375673cab3474ff735a6bbe2db4f2eeb0155
Simon Hildebrandt <simonhildebrandt@gmail.com>
2022-09-12 12:40:30 +1000

    Basic logic for engaging with BLE

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
 import "@babel/polyfill";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => "Rendered!";
+
+navigator.bluetooth.getAvailability().then(result => console.log('bluetooth?', {result}));
+
+const App = () => {
+  const findMatches = () => {
+    navigator.bluetooth.requestDevice({acceptAllDevices: true }).then(
+      devs => {
+        console.log({devs});
+      }
+    );
+  }
+
+  return <div><button onClick={findMatches}>Match</button></div>
+};
 
 ReactDOM.render(<App/>, document.getElementById('app'));
